efdc060080988498b1072385c831bb6161e4ef4a
tanetakumi <nutanetakumi@yahoo.co.jp>
2022-05-26 15:26:19 +0000

    Change .gitignore

--- scripts/optimize/optimize/calculator.py
+++ scripts/optimize/optimize/calculator.py
@@ -1,5 +1,8 @@
 import math
 import re
+import numpy as np
+from scipy.stats import truncnorm
+
 
 def betac(calc_type: str, area: float = None, Rshunt: float= None, Rsubgap: float = 100, Cap : float = 0.064e-12, Ic: float = 0.1e-3, betac: float = 1) -> float:
     phi = 2.068 * 10 ** -15
@@ -42,3 +45,17 @@ def shunt_calc(area: float = None, Rsubgap: float = 100, Cap : float = 0.064e-12
     _Rsubgap = Rsubgap / area
     denomi = math.sqrt(2 * math.pi * _Ic * _Cap / (phi * betac)) - 1/_Rsubgap
     return 1/denomi
+
+
+def rand_norm(mean, std, upper = None, lower = None):
+    if upper == None:
+        b = np.inf
+    else:
+        b = (upper - mean) / std
+
+    if lower == None:
+        a = -np.inf
+    else:
+        a = (lower - mean) / std 
+
+    return truncnorm(a, b, loc=100, scale=10).rvs()
