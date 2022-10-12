3eda61b89bc288d8eba631736ff9caa3332c53ce
tanetakumi <tanetakumi@gmail.com>
2022-10-12 13:55:54 +0000

    add betac_calc

--- scripts/optimize/optimize/calculator.py
+++ scripts/optimize/optimize/calculator.py
@@ -39,7 +39,7 @@ def betac(calc_type: str, area: float = None, Rshunt: float= None, Rsubgap: floa
         raise ValueError('calc_type(計算タイプ)の値が読み取れません。')
 
 
-def shunt_calc(area: float = None, Rsubgap: float = 100, Cap : float = 0.064e-12, Ic: float = 0.1e-3, betac: float = 1) -> float:
+def shunt_calc(area: float, Rsubgap: float = 100, Cap : float = 0.064e-12, Ic: float = 0.1e-3, betac: float = 1) -> float:
     phi = 2.068 * 10 ** -15
     _Cap = Cap * area
     _Ic = Ic * area
@@ -47,6 +47,14 @@ def shunt_calc(area: float = None, Rsubgap: float = 100, Cap : float = 0.064e-12
     denomi = math.sqrt(2 * math.pi * _Ic * _Cap / (phi * betac)) - 1/_Rsubgap
     return 1/denomi
 
+def betac_calc(area: float, Rshunt: float, Rsubgap: float = 100, Cap : float = 0.064e-12, Ic: float = 0.1e-3) -> float:
+    phi = 2.068 * 10 ** -15
+    _Cap = Cap * area
+    _Ic = Ic * area
+    _Rsubgap = Rsubgap / area
+    Rj = Rshunt * _Rsubgap/(Rshunt + _Rsubgap)
+
+    return (2 * math.pi * _Ic * _Cap * Rj**2) / phi
 
 def rand_norm(mean, std, upper = None, lower = None):
     if upper == None:
